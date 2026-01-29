24ad952bcac769b7f5fbffa22600ac265c585a6b
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-29 22:41:44 +0500

    Court officer  Links

--- src/config/sidebar/court-officer.js
+++ src/config/sidebar/court-officer.js
@@ -2,6 +2,7 @@ import {
   LayoutDashboard,
   Gavel,
   CalendarDays,
+  UserPen,
 } from "lucide-react";
 
 export const courtOfficerSidebarItems = [
@@ -9,15 +10,21 @@ export const courtOfficerSidebarItems = [
     title: "Dashboard",
     url: "/dashboard/court-officer",
     icon: LayoutDashboard,
+    exact: true,
   },
   {
     title: "Assigned Cases",
     url: "/dashboard/court-officer/cases",
     icon: Gavel,
   },
-  {
-    title: "Hearings",
-    url: "/dashboard/court-officer/hearings",
-    icon: CalendarDays,
+  // {
+  //   title: "Hearings",
+  //   url: "/dashboard/court-officer/hearings",
+  //   icon: CalendarDays,
+  // },
+   {
+    title: "Profile",
+    url: "/dashboard/court-officer/profile",
+    icon: UserPen,
   },
 ];
