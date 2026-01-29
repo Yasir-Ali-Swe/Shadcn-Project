886afa22ca2925fe697b623c8993b8bf17cea692
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-29 21:21:00 +0500

    Roles Links

---
+++ src/lib/sidbar.js
@@ -0,0 +1,23 @@
+// src/lib/sidebar.js
+import { clientSidebarItems } from "@/config/sidebar/client";
+import { lawyerSidebarItems } from "@/config/sidebar/lawyer";
+import { clerkSidebarItems } from "@/config/sidebar/clerk";
+import { courtOfficerSidebarItems } from "@/config/sidebar/court-officer";
+import { adminSidebarItems } from "@/config/sidebar/admin";
+
+export const getSidebarItemsByRole = (role) => {
+  switch (role) {
+    case "client":
+      return clientSidebarItems;
+    case "lawyer":
+      return lawyerSidebarItems;
+    case "clerk":
+      return clerkSidebarItems;
+    case "courtOfficer":
+      return courtOfficerSidebarItems;
+    case "admin":
+      return adminSidebarItems;
+    default:
+      return [];
+  }
+};
