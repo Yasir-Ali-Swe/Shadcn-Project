73a8ca348ee09d253054773799851dc40ede2ac3
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-30 21:21:50 +0500

    Dashboard Layout

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,26 +1,23 @@
 import React from "react";
-import { SidebarProvider} from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import DashboardSidebar from "@/components/dashboard-sidebar";
 import DasboardNavbar from "@/components/dasboard-navbar";
+import AuthGuard from "@/components/auth/AuthGuard";
+import { SocketProvider } from "@/providers/socket-provider";
 import { cookies } from "next/headers";
 
-const layout = async ({ children }) => {
-  const cookieStore = await cookies();
-  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+export default function DashboardLayout({ children }) {
   return (
-    <SidebarProvider
-      style={{
-        "--sidebar-width": "14rem",
-      }}
-      defaultOpen={defaultOpen}
-    >
-      <DashboardSidebar />
-      <div className="flex flex-col w-screen">
-        <DasboardNavbar />
-        <main className="flex-1">{children}</main>
-      </div>
+    <SidebarProvider>
+      <AuthGuard>
+        <SocketProvider>
+          <DashboardSidebar />
+          <div className="flex flex-col w-screen">
+            <DasboardNavbar />
+            <main className="flex-1 m-3">{children}</main>
+          </div>
+        </SocketProvider>
+      </AuthGuard>
     </SidebarProvider>
   );
-};
-
-export default layout;
+}
