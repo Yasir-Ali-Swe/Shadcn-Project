6c1ae26e0aec65dabc7d5a1c488c1b381e9f699b
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-01 00:24:14 +0500

    Update main app layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "@/components/providers/auth-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider>{children}</Provider>
+        <Provider>
+          {children}
+          <Toaster position="top-right" />
+        </Provider>
       </body>
     </html>
   );
