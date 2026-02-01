bf2ac78912dfa155f3ae74084b4dcb119e7c39a3
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-02 00:28:29 +0500

    Update public layout

--- src/app/(public)/layout.jsx
+++ src/app/(public)/layout.jsx
@@ -4,9 +4,9 @@ import Footer from "@/components/footer";
 
 const layout = ({ children }) => {
   return (
-    <div className="w-screen h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="px-15 flex-1 overflow-x-hidden">{children}</main>
+      <main className="px-4 md:px-12 flex-1 overflow-x-hidden">{children}</main>
       <Footer />
     </div>
   );
