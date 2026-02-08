6c84d5d0d8ffa9a36e8485b25488178d6488ab0e
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-08 15:32:40 +0500

    Update auth layout

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const layout = ({ children }) => {
   return (
-    <main className="min-h-screen w-full flex justify-center items-center p-4">
+    <main className="min-h-screen w-full flex justify-center items-center">
       {children}
     </main>
   );
